62ef996b1a752462065413551b6889c6689b6342
William Sia <williamsia82@gmail.com>
2015-07-11 08:28:52 +0800

    Add some logic check to email submission site

--- client/app/kritik/kritik.controller.js
+++ client/app/kritik/kritik.controller.js
@@ -4,13 +4,23 @@ angular.module('kritikWebsiteApp')
   .controller('KritikCtrl', function ($scope, $http, socket) {
     $scope.errors = {};
 
-    $scope.addWebsite = function() {
-        if($scope.form === undefined) {
-            return;
-        }
-        $http.post('/api/websites', $scope.website );
-        $scope.form = undefined;
-    };
+    $scope.addWebsite = function(form) {
 
+        $scope.submitted = true;
+
+        if(form.$valid) {
+
+            $http.post('/api/websites', $scope.website)
+                .success(function(){
+                    $scope.successfullSubmission = true;
+                    $scope.website = undefined;         
+                    $scope.submitted = false;          
+                })
+                .error(function(){
+                    $scope.successfullSubmission = false;                   
+                });
+        }
+
+    };
 
   });
